c971b98b45f7d0c2955485db946bd32e32e5b876
Ryuken13 <daniel.shahroozi@gmail.com>
2018-09-12 10:17:05 +0200

    Läste in user_input namn

--- src/UppgiftONSDAG.java
+++ src/UppgiftONSDAG.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 public class UppgiftONSDAG {
 
 	public static void main(String[] args) {
+
 		Scanner input = new Scanner(System.in);
+		System.out.println("Vad heter du?");
+
+		String name = input.nextLine();
+		
+		
+
 	}
+
 }
