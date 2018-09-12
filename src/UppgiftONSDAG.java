831dd5d8aa1107929c2c3dbb22d89fec4be70b61
Ryuken13 <daniel.shahroozi@gmail.com>
2018-09-12 11:29:01 +0200

    läste in användarens information

--- src/UppgiftONSDAG.java
+++ src/UppgiftONSDAG.java
@@ -5,13 +5,24 @@ public class UppgiftONSDAG {
 	public static void main(String[] args) {
 
 		Scanner input = new Scanner(System.in);
+		
 		System.out.println("Vad heter du?");
-
 		String name = input.nextLine();
-		
+
 		System.out.println("age?");
+		String age = input.nextLine();
 		
-		String age = input.nextLine();		
+		System.out.println("Addres?");
+		String adress = input.nextLine();
+	
+		System.out.println("Postnummer?");
+		String Postnummer = input.nextLine();
+
+		System.out.println("Ort?");
+		String ort = input.nextLine();
+
+		System.out.println("Telefonnummer?");
+		String telefonnummer = input.nextLine();
 
 	}
 
