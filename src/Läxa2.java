958e5ee032ec79a5e6ede7431c0e233c213740ef
Ryuken13 <daniel.shahroozi@gmail.com>
2018-09-14 15:07:36 +0200

    skapde 10 st variabler

---
+++ src/Läxa2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Läxa2 {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		// Läs in tal 1
+		int tal1 = input.nextInt();
+		int min = tal1;
+		int max = tal1;
+		
+		// Läs in tal 2
+		int tal2 = input.nextInt();
+		if(tal2 > max){
+			max = tal2;
+		}
+		if()
+		int tal3 = input.nextInt();
+		int tal4 = input.nextInt();
+		int tal5 = input.nextInt();
+		int tal6 = input.nextInt();
+		int tal7 = input.nextInt();
+		int tal8 = input.nextInt();
+		int tal9 = input.nextInt();
+		int tal10 = input.nextInt();
+		
+	}
+
+}
