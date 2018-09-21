12d6f6d1fcdf3271a3d8b42e422e5d544f1af9d6
Ryuken13 <daniel.shahroozi@gmail.com>
2018-09-21 14:43:54 +0200

    Done

---
+++ src/Kattis.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+
+public class Kattis {
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		int r2;
+		int s = input.nextInt();
+		int r1 = input.nextInt();
+		
+		
+		System.out.println(r1 * 2 -s);
+		
+	}
+
+}
---
+++ src/Kattis2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Kattis2 {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		
+		int x=1;
+		int i= input.nextInt();
+		
+		while(x <=i){
+			System.out.println(x + " " + "Abracadabra");
+			
+			x++;
+		}
+	}
+}
